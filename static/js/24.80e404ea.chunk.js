e6a41ca7d139b2d364205fd57f97bc2a40a2ce08
Serhiy Korytskyy <korysergey55@gmail.com>
2024-03-01 10:57:57 +0200

    Updates

--- static/js/24.80e404ea.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfurniture=self.webpackChunkfurniture||[]).push([[24],{3024:(s,e,c)=>{c.r(e),c.d(e,{default:()=>n});c(9060);const r={};var a=c(2496);const n=()=>(0,a.jsx)("div",{className:r.products,children:(0,a.jsx)("div",{className:r.comtainer,children:(0,a.jsx)("h2",{children:"ProcessPage"})})})}}]);
-//# sourceMappingURL=24.80e404ea.chunk.js.map
