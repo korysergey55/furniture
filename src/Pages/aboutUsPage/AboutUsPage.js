b72dead178f6c2c84b715d7b9152530a40e7fd17
Serhiy Korytskyy <korysergey55@gmail.com>
2024-03-19 20:55:52 +0200

    process-photo

--- src/Pages/aboutUsPage/AboutUsPage.js
+++ src/Pages/aboutUsPage/AboutUsPage.js
@@ -9,9 +9,10 @@ const AboutUs = () => {
         <h2 className={styles.title}>About Us</h2>
         <p className={styles.discription}>
           Vitaly furniture is a small family business that was founded in 2017. Our commitment to innovation in
-          production is the key to the best quality product. We are passionate about making furniture.
-          We have implemented many projects developed in collaboration with our in-house designers and
-          architects in the private and commercial sector.
+          production is the key to the best quality product. We have implemented many projects developed in collaboration with our in-house designers and architects. We are a bespoke furniture & joinery company, that prides ourselves on providing only the very best quality products and service to our clients. From design to installation we take care of everything ensuring impeccable quality for both product and service. Services Provided 3D design and interior design Free standing and fitted furniture. First, second and final fix joinery.
+          <br />
+          Areas Served
+          Cheshire, Manchester mainly but also nationwide in the private and commercial sector.
         </p>
       </div>
     </div>
--- src/utiles/contactInfo/contactInfo.js
+++ src/utiles/contactInfo/contactInfo.js
@@ -4,6 +4,6 @@ const contactInfo = {
   address: 'Manchester Man and Van',
   instagram: 'https://www.instagram.com/vitaly_furniture?igsh=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr',
   facebook: 'https://www.facebook.com/',
-  location: 'https://maps.app.goo.gl/HxPhiiuEMtVVrEKU9'
+  location: 'https://www.google.com/maps?q=53.3892599,-2.2015907&hl=ru-GB&gl=uk&entry=gps&lucs=,94207808,47071704,47069508,47084304&g_ep=CAISDDYuOTMuMy4yNTkyMBgAINeCAyokLDk0MjA3ODA4LDQ3MDcxNzA0LDQ3MDY5NTA4LDQ3MDg0MzA0QgJHQg%3D%3D&g_st=it'
 }
 export { contactInfo }
